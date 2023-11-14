e3897137e6f4a31ab944697c1cbd21cfb005bc49
alexstaniea <alex.staniea@gmail.com>
2023-11-14 18:05:27 +0200

    Lesson 2 - Basic ToDo App

--- ToDoIfy/MainPage.xaml.cs
+++ ToDoIfy/MainPage.xaml.cs
@@ -1,25 +1,40 @@
-﻿namespace ToDoIfy;
+﻿using ToDoIfy.ViewModels;
+
+namespace ToDoIfy;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private ToDoViewModel _viewModel;
 
-	public MainPage()
+    public MainPage()
 	{
 		InitializeComponent();
-	}
+        _viewModel = new ToDoViewModel();
+        BindingContext = _viewModel;
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnAddClicked(object sender, EventArgs e)
+    {
+        var newTaskTitle = NewTaskEntry.Text;
+        if (!string.IsNullOrWhiteSpace(newTaskTitle))
+        {
+            _viewModel.AddTodoItem(newTaskTitle);
+            NewTaskEntry.Text = string.Empty;
+        }
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnRemoveClicked(object sender, EventArgs e)
+    {
+        var button = (Button)sender;
+        if (button?.CommandParameter is TodoItem todoItem)
+        {
+            _viewModel.RemoveTodoItem(todoItem);
+        }
+    }
 }
 
-
+public class TodoItem
+{
+	public string Title { get; set; }
+    public bool IsDone { get; set; }
+}
---
+++ ToDoIfy/ViewModels/ToDoViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ToDoIfy.ViewModels
+{
+	public class ToDoViewModel : BindableObject
+	{
+
+        private ObservableCollection<TodoItem> _todoItems;
+
+        public ObservableCollection<TodoItem> TodoItems
+        {
+            get => _todoItems;
+            set
+            {
+                _todoItems = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ToDoViewModel()
+		{
+            TodoItems = new ObservableCollection<TodoItem>
+            {
+                new TodoItem { Title = "Task 1", IsDone = false },
+                new TodoItem { Title = "Task 2", IsDone = true },
+                new TodoItem { Title = "Task 3", IsDone = false }
+            };
+        }
+
+        public void AddTodoItem(string title)
+        {
+            TodoItems.Add(new TodoItem { Title = title, IsDone = false });
+        }
+
+        public void RemoveTodoItem(TodoItem todoItem)
+        {
+            TodoItems.Remove(todoItem);
+        }
+    }
+}
+
