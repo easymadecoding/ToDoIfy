0e16a9fe55e38b87524b3af1c53cdc1f6cb3f1df
alexstaniea <alex.staniea@gmail.com>
2023-11-30 11:53:03 +0200

    Lesson 9 - Add Blur In Task Manager

--- ToDoIfy/MauiProgram.cs
+++ ToDoIfy/MauiProgram.cs
@@ -1,5 +1,14 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.LifecycleEvents;
 using ToDoIfy.ViewModels;
+
+#if ANDROID
+using Android.Views;
+#endif
+
+#if IOS
+using UIKit;
+#endif
 
 namespace ToDoIfy;
 
@@ -14,13 +23,48 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+            })
+        .ConfigureLifecycleEvents(events =>
+        {
+
+#if ANDROID
+		events.AddAndroid(android => android.OnPause((activity) =>
+			activity.Window?.SetFlags(WindowManagerFlags.Secure, WindowManagerFlags.Secure)));
+        events.AddAndroid(android => android.OnResume ((activity) =>
+			activity.Window?.ClearFlags(WindowManagerFlags.Secure)));
+#endif
+
+
+#if IOS
+				UIVisualEffectView _blurWindow = null;
+
+				events.AddiOS(ios => ios.OnResignActivation((application) =>
+				{
+					using (var blurEffect = UIBlurEffect.FromStyle(UIBlurEffectStyle.Dark))
+					{
+						_blurWindow = new UIVisualEffectView(blurEffect)
+						{
+							Frame = UIApplication.SharedApplication.KeyWindow.RootViewController.View.Bounds
+						};
+
+						UIApplication.SharedApplication.KeyWindow.RootViewController.View.AddSubview(_blurWindow);
+					}
+				}));
+
+				events.AddiOS(ios => ios.OnActivated((application) =>
+				{
+					_blurWindow?.RemoveFromSuperview();
+					_blurWindow?.Dispose();
+					_blurWindow = null;
+				}));
+#endif
+	});
 
 #if DEBUG
 		builder.Logging.AddDebug();
 #endif
 
-		builder.Services.AddSingleton<MainPage>();
+        builder.Services.AddSingleton<MainPage>();
         builder.Services.AddSingleton<ToDoViewModel>();
 
         builder.Services.AddTransient<DetailsPage>();
