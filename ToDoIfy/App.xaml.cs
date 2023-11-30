a27460783ea20967f19d69e17d55a01d5260d6d8
alexstaniea <alex.staniea@gmail.com>
2023-11-30 13:52:46 +0200

    Lesson 10 - Add Face ID

--- ToDoIfy/App.xaml.cs
+++ ToDoIfy/App.xaml.cs
@@ -1,12 +1,43 @@
-﻿namespace ToDoIfy;
+﻿
+using Plugin.Fingerprint;
+using Plugin.Fingerprint.Abstractions;
+
+namespace ToDoIfy;
 
 public partial class App : Application
 {
 	public App()
 	{
 		InitializeComponent();
-
-		MainPage = new AppShell();
+        MainPage = new AppShell();
 	}
-}
 
+    protected override async void OnStart()
+	{
+        await CheckFaceIdAuth();
+    }
+
+    protected override async void OnResume()
+	{
+        await CheckFaceIdAuth();
+    }
+
+	private async Task CheckFaceIdAuth()
+	{
+        var authenticationRequest = new
+        AuthenticationRequestConfiguration("Are you authorized?",
+        "We need to ensure it is you before proceeding");
+
+        var authenticationResult =
+            await CrossFingerprint.Current.AuthenticateAsync(authenticationRequest);
+
+        if (authenticationResult.Authenticated)
+        {
+            MainPage = new AppShell();
+        }
+        else
+        {
+            System.Environment.Exit(0);
+        }
+    }
+}
